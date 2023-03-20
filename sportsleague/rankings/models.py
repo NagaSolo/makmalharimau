09e8326ce2d09d59f4b15616259da9f8eb4138cc
NagaSolo <muadz.nagasolo@gmail.com>
2023-03-20 13:26:35 +0800

    modified, added, tested; erd, models, testcases

--- sportsleague/rankings/models.py
+++ sportsleague/rankings/models.py
@@ -1,3 +1,35 @@
+
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    filename = models.CharField(max_length=128)
+    status = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=32)
+
+    def points_count(self):
+        overall_points = 0
+        gamepoints = GamePoint.objects.select_related('team').filter(team__name__iexact=self.name)
+        for gamepoint in gamepoints:
+            overall_points += gamepoint.point
+        return overall_points
+        
+
+class Game(models.Model):
+    team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team_1")
+    team_2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team_2")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.team_1.name + ' vs ' + self.team_2.name + ' at ' + self.timestamp.astimezone
+
+
+class GamePoint(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="game")
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team")
+    point = models.IntegerField(blank=True, null=True)
--- sportsleague/rankings/tests.py
+++ sportsleague/rankings/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Game, Team, GamePoint, File
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username='harimau', password='hentam')
+        User.objects.create(username='singa', password='keromo')
+
+    def test_user_is_created(self):
+        harimau = User.objects.get(username='harimau', password='hentam')
+        singa = User.objects.get(username='singa', password='keromo')
+        self.assertEqual(harimau.username,'harimau')
+        self.assertEqual(singa.username,'singa')
+
+
+class FileTestCase(TestCase):
+    # testing File model with status Failed or Success
+    def setUp(self):
+        File.objects.create(filename='fantasy_league_1.csv', status=True)
+        File.objects.create(filename='fantasy_league_1.xlsx')
+
+    def test_file_reference(self):
+        success = File.objects.get(filename='fantasy_league_1.csv')
+        failed = File.objects.get(filename='fantasy_league_1.xlsx')
+        self.assertTrue(success.status,True)
+        self.assertFalse(failed.status,True)
+
+
+class RankingTestCase(TestCase):
+    # testing team creation, game creation, and point award
+    def setUp(self):
+        crazy = Team.objects.create(name='Crazy Ones')
+        rebs = Team.objects.create(name='Rebels')
+
+        Game.objects.create(team_1=crazy, team_2=rebs)
+        Game.objects.create(team_1=rebs, team_2=crazy)
+
+    def test_team_is_created(self):
+        team_1 = Team.objects.get(name='Crazy Ones')
+        team_2 = Team.objects.get(name='Rebels')
+        self.assertEqual(team_1.name,'Crazy Ones')
+        self.assertEqual(team_2.name,'Rebels')
+
+    def test_game_creation(self):
+        crazy = Team.objects.get(name='Crazy Ones')
+        rebs = Team.objects.get(name='Rebels')
+
+        game_1 = Game.objects.get(team_1=crazy, team_2=rebs)
+        game_2 = Game.objects.get(team_1=rebs, team_2=crazy)
+
+        self.assertEqual(game_1.team_1.name,'Crazy Ones')
+        self.assertEqual(game_1.team_2.name,'Rebels')
+        self.assertEqual(game_2.team_1.name,'Rebels')
+        self.assertEqual(game_2.team_2.name,'Crazy Ones')
+
+    def test_point_award(self):
+        crazy = Team.objects.get(name='Crazy Ones')
+        rebs = Team.objects.get(name='Rebels')
+
+        game_1 = Game.objects.get(team_1=crazy, team_2=rebs)
+        game_2 = Game.objects.get(team_1=rebs, team_2=crazy)
+
+        game1_team1_point = GamePoint.objects.create(game=game_1, team=crazy, point=3)
+        game1_team2_point = GamePoint.objects.create(game=game_1, team=rebs, point=0)
+
+        game2_team1_point = GamePoint.objects.create(game=game_2, team=rebs, point=0)
+        game2_team2_point = GamePoint.objects.create(game=game_2, team=crazy, point=3)
+        
+        self.assertEqual(game1_team1_point.point,3)
+        self.assertEqual(game1_team2_point.point,0)
+        self.assertEqual(game2_team1_point.point,0)
+        self.assertEqual(game2_team2_point.point,3)
+
+        self.assertEqual(crazy.points_count(), 6)
+        self.assertEqual(rebs.points_count(), 0)
